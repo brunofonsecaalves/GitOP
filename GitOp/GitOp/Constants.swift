622c9329ad24af69cbc0534296c6400c2082cf37
Bruno Fonseca Alves <76008140+brunofonsecaalves@users.noreply.github.com>
2021-02-08 16:24:33 -0300

    Add files via upload

---
+++ GitOp/GitOp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitOp
+//
+//  Created by Bruno Alves on 05/02/21.
+//
+
+import Foundation
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+}
---
+++ GitOp/GitOp/RepositoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryCell.swift
+//  GitOp
+//
+//  Created by Bruno Alves on 05/02/21.
+//
+
+import Foundation
+
+struct RepositoryCell {
+    let title: String
+    let text: String
+}
